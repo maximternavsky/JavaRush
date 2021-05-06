265f324a51098445daed3ec29f4cc1414b29e565
maximternavsky <maxim.ternavsky@gmail.com>
2021-05-06 22:52:02 +0300

    solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0802/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task08.task0802;
+
+/* 
+Утильный класс: часть 2
+*/
+
+public class Solution {
+
+    public static double sqrt(double a) {
+        return a * a;
+    }
+
+    public static double cbrt(double a) {
+        return a * a * a;
+    }
+
+    public static double pow(int number, int power) {
+        int result = number;
+        if(power == 0) {
+            return 1;
+        }
+        for(int i = 1; i < power; i++) {
+            result *= number;
+        }
+        return power < 0 ? 1d / result : result;
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0915/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task09.task0915;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/* 
+StringTokenizer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String packagePath = "com.javarush.task.pro.task09.task0915";
+        String[] tokens = getTokens(packagePath, ".a");
+        System.out.println(Arrays.toString(tokens));
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        //напишите тут ваш код
+        return null;
+    }
+}
